c1152060c6964b5c040c3b83200cf0976eb497fd
Utarn Buranasaksri <utarn@zarimpun.com>
2020-09-09 05:44:58 +0700

    Finish Project

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using aspnetcore_qrcode.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(aspnetcore_qrcode.Areas.Identity.IdentityHostingStartup))]
+namespace aspnetcore_qrcode.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
